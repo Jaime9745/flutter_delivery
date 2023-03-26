98390109b620fd78830a9209c0c7d5172b41dd08
Jaime PC <jaimedanielmontana@gmail.com>
2023-03-26 10:56:32 -0500

    Color

--- lib/src/utils/my_colors.dart
+++ lib/src/utils/my_colors.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class MyColors {
-  static Color primaryColor = Color(0xFFE70D32);
+  static Color primaryColor = Color(0xFFA7001c);
   static Color primaryColorDark = Color(0xFFA7001c);
   static Color primaryOpacityColor = Color.fromRGBO(231, 13, 50, 0.09);
 }
