ad9ee63155787189173a44c862c147fdcf6d0ccd
Jaime Huawei <jaimedanielmontana@gmail.com>
2023-03-27 08:15:35 -0500

    default

--- lib/src/pages/login/login_controller.dart
+++ lib/src/pages/login/login_controller.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_delivery/src/models/response_api.dart';
 import 'package:flutter_delivery/src/provider/user_provider.dart';
-import 'package:flutter_delivery/src/utils/my_snackbar.dart';
-import 'package:flutter_delivery/src/utils/shared_pref.dart';
-
-import '../../models/user.dart';
 
 class LoginController{
   BuildContext context;
@@ -12,7 +8,6 @@ class LoginController{
   TextEditingController passwordController = new TextEditingController();
 
   UserProvider usersProvider = new UserProvider();
-  SharedPref _sharedPref = new SharedPref();
 
   Future init(BuildContext context) async{
     this.context = context;
@@ -27,18 +22,7 @@ class LoginController{
     String email = emailController.text.trim();
     String password = passwordController.text.trim();
 
-    ResponseApi responseApi = await usersProvider.login(email, password);
-
-    if(responseApi.success){
-      User user = User.fromJson(responseApi.data);
-      _sharedPref.save('user', user.toJson());
-    }
-    else{
-      MySnackbar.show(context, responseApi.message);
-    }
-    print('Respuesta object: ${responseApi}');
-    print('Respuesta: ${responseApi.toJson()}');
-    MySnackbar.show(context, responseApi.message);
+    ResponseApi responseApi = awai
     print('Email: $email');
     print('Password: $password');
   }
